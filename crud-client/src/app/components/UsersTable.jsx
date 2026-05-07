1e9d4b4d944508b8746fc5580358e531169b5872
alfu-git <mdalfaz.dev@gmail.com>
2026-05-07 23:31:48 +0600

    display users data in client-side ui

---
+++ crud-client/src/app/components/UsersTable.jsx
@@ -0,0 +1,40 @@
+import { Button, Table } from "@heroui/react";
+import React from "react";
+
+const UsersTable = ({ users }) => {
+  console.log(users);
+  return (
+    <Table>
+      <Table.ScrollContainer>
+        <Table.Content aria-label="Team members" className="min-w-[600px]">
+          <Table.Header>
+            <Table.Column isRowHeader>Name</Table.Column>
+            <Table.Column>Role</Table.Column>
+            <Table.Column>Status</Table.Column>
+            <Table.Column>Email</Table.Column>
+          </Table.Header>
+
+          <Table.Body>
+            {users.map((user) => (
+              <Table.Row key={user._id}>
+                <Table.Cell>{user.name}</Table.Cell>
+                <Table.Cell>{user.role}</Table.Cell>
+                <Table.Cell>{user.email}</Table.Cell>
+
+                <Table.Cell className={"flex flex-wrap gap-4"}>
+                  <Button variant="outline">Details</Button>
+
+                  <Button>Edit</Button>
+
+                  <Button variant="danger">Delete</Button>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Content>
+      </Table.ScrollContainer>
+    </Table>
+  );
+};
+
+export default UsersTable;
--- crud-client/src/app/users/page.jsx
+++ crud-client/src/app/users/page.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { getUsers } from "../lib/data";
+import UsersTable from "../components/UsersTable";
 
-const UsersPage = () => {
+const UsersPage = async () => {
+  const users = await getUsers();
+
   return (
     <section className="my-10 container mx-auto px-5">
       <div>
         <h2 className="mb-5 text-3xl font-semibold text-center">
           Users Management
         </h2>
+
+        <div>
+          <UsersTable users={users} />
+        </div>
       </div>
     </section>
   );
