c1f8a3b69853a0d0228916949da102cd1888e7b2
alfu-git <mdalfaz.dev@gmail.com>
2026-05-07 19:45:52 +0600

    connected server to mongodb

--- crud-express-server/index.js
+++ crud-express-server/index.js
@@ -1,11 +1,37 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://module-50-user:BUohT5gKi8TO1BQw@alfu-cluster7.go9kvu6.mongodb.net/?appName=alfu-cluster7`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World! ");
